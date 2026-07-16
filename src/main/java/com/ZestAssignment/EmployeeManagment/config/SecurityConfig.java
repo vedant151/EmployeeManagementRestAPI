2542dd3a1d7a9b665f9f806ba6a1213964dbf2f2
Vedant Sabde <vedant.sabde20@vit.edu>
2026-07-16 10:35:51 +0530

    code

--- src/main/java/com/ZestAssignment/EmployeeManagment/config/SecurityConfig.java
+++ src/main/java/com/ZestAssignment/EmployeeManagment/config/SecurityConfig.java
@@ -42,23 +42,7 @@ public class SecurityConfig {
 
                         .requestMatchers(HttpMethod.GET, "/api/login", "/api/signup").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/login", "/api/signup").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/req/signup/**", "/req/signup").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/home").permitAll()
 
-                        // organizer only
-                        .requestMatchers(HttpMethod.GET,   "/api/getEmployee/").hasRole("ORGANIZER")
-                        .requestMatchers(HttpMethod.PUT,    "/api/event/**").hasRole("ORGANIZER")
-                        .requestMatchers(HttpMethod.POST,   "/api/createEmployee").hasRole("ORGANIZER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/delete/**").hasRole("ORGANIZER")
-
-                        // employee only
-                        .requestMatchers(HttpMethod.GET, "/api/EmployeeId/**").hasRole("EMPLOYEE")
-
-
-
-                        .requestMatchers(HttpMethod.POST, "/api/logout").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/user/role").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/home").permitAll()
 
                         .anyRequest().authenticated()
                 )
